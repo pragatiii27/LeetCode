40d2c6f6567335e472c36b25f495867535e1fc4b
pragatiii27 <pragatisuryawanshi2707@gmail.com>
2026-07-06 22:22:15 +0530

    Sync: 10. Regular Expression Matching (Hard) - Leet2Git

---
+++ 10_Regular_Expression_Matching.java
@@ -0,0 +1,38 @@
+class Solution {
+
+    private Boolean[][] memo;
+
+    public boolean isMatch(String s, String p) {
+        memo = new Boolean[s.length() + 1][p.length() + 1];
+        return dfs(0, 0, s, p);
+    }
+
+    private boolean dfs(int i, int j, String s, String p) {
+
+        if (memo[i][j] != null) {
+            return memo[i][j];
+        }
+
+        if (j == p.length()) {
+            return memo[i][j] = (i == s.length());
+        }
+
+        boolean firstMatch =
+                (i < s.length()) &&
+                (s.charAt(i) == p.charAt(j) || p.charAt(j) == '.');
+
+        boolean ans;
+
+        if (j + 1 < p.length() && p.charAt(j + 1) == '*') {
+
+            ans = dfs(i, j + 2, s, p) ||    // skip x*
+                  (firstMatch && dfs(i + 1, j, s, p)); // use x*
+
+        } else {
+
+            ans = firstMatch && dfs(i + 1, j + 1, s, p);
+        }
+
+        return memo[i][j] = ans;
+    }
+}
