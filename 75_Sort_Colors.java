1373808126bc4a1bb84c36bc4c2c4b38c31e397c
pragatiii27 <pragatisuryawanshi2707@gmail.com>
2026-07-11 19:09:28 +0530

    Sync: 75. Sort Colors (Medium) - Leet2Git

--- 75_Sort_Colors.java
+++ 75_Sort_Colors.java
@@ -1,19 +1,24 @@
 class Solution {
+    private void swap(int[] nums, int a , int b){
+        int temp= nums[a];
+        nums[a]= nums[b];
+        nums[b]= temp;
+    }
     public void sortColors(int[] nums) {
-       
-        int n=nums.length;
-        int left= 0;
-        int right= n-1;
-
-        while(left<n && right>0){
-            if(nums[left]> nums[right]){
-                int temp = nums[left];
-                nums[left] = nums[right];
-                nums[right] = temp;
-
+        int low=0, mid=0, high= nums.length -1;
+        while(mid<=high){
+            if(nums[mid]==0){
+                swap(nums, low, mid);
+                low++;
+                mid++;
             }
-            left++;
-            right--;
+            else if(nums[mid]==1){
+                mid++;
+            }
+            else{
+                swap(nums, mid, high);
+                high--;
+            }
         }
     }
 }
