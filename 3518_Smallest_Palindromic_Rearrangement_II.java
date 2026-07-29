4fd4137d23a9429335c389a332cd8b2dd521181a
pragatiii27 <pragatisuryawanshi2707@gmail.com>
2026-07-29 22:35:29 +0530

    Sync: 3518. Smallest Palindromic Rearrangement II (Hard) - Leet2Git

--- 3518_Smallest_Palindromic_Rearrangement_II.java
+++ 3518_Smallest_Palindromic_Rearrangement_II.java
@@ -1,77 +1,76 @@
-import java.math.BigInteger;
-
 class Solution {
 
-    BigInteger[] fact = new BigInteger[55];
+    long LIMIT;
 
     public String smallestPalindrome(String s, int k) {
+        LIMIT = k;
 
-        int[] freq = new int[26];
-
-        for(char c : s.toCharArray())
-            freq[c-'a']++;
+        int[] cnt = new int[26];
+        for (char c : s.toCharArray())
+            cnt[c - 'a']++;
 
         String mid = "";
-
         int[] half = new int[26];
-        int len = 0;
+        int m = 0;
 
-        for(int i=0;i<26;i++){
+        for (int i = 0; i < 26; i++) {
+            if ((cnt[i] & 1) == 1)
+                mid = String.valueOf((char) ('a' + i));
 
-            if(freq[i]%2==1)
-                mid = ""+(char)('a'+i);
-
-            half[i]=freq[i]/2;
-            len+=half[i];
+            half[i] = cnt[i] / 2;
+            m += half[i];
         }
 
-        fact[0]=BigInteger.ONE;
-        for(int i=1;i<=50;i++)
-            fact[i]=fact[i-1].multiply(BigInteger.valueOf(i));
-
-        BigInteger total=countWays(half,len);
-
-        if(total.compareTo(BigInteger.valueOf(k))<0)
+        if (countWays(half, m) < k)
             return "";
 
-        StringBuilder first=new StringBuilder();
+        StringBuilder first = new StringBuilder();
 
-        while(len>0){
+        while (m > 0) {
 
-            for(int c=0;c<26;c++){
+            for (int c = 0; c < 26; c++) {
 
-                if(half[c]==0) continue;
+                if (half[c] == 0)
+                    continue;
 
                 half[c]--;
 
-                BigInteger ways=countWays(half,len-1);
+                long ways = countWays(half, m - 1);
 
-                if(ways.compareTo(BigInteger.valueOf(k))>=0){
-
-                    first.append((char)('a'+c));
-                    len--;
+                if (ways >= k) {
+                    first.append((char) ('a' + c));
+                    m--;
                     break;
-
-                }else{
-
-                    k-=ways.intValue();
+                } else {
+                    k -= ways;
                     half[c]++;
-
                 }
             }
         }
 
-        String second=new StringBuilder(first).reverse().toString();
+        String second = new StringBuilder(first).reverse().toString();
 
-        return first.toString()+mid+second;
+        return first.toString() + mid + second;
     }
 
-    private BigInteger countWays(int[] half,int len){
+    private long countWays(int[] half, int total) {
 
-        BigInteger ans=fact[len];
+        long ans = 1;
+        int remain = total;
 
-        for(int x:half)
-            ans=ans.divide(fact[x]);
+        for (int i = 0; i < 26; i++) {
+
+            int f = half[i];
+
+            for (int j = 1; j <= f; j++) {
+                ans = ans * remain / j;
+
+                if (ans > LIMIT)
+                    return LIMIT;
+
+                remain--;
+            }
+        }
 
         return ans;
     }
